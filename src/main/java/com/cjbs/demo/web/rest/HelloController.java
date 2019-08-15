d6dc7a0a2bc0f65a58d6443bf03b01f0b5c5501b
shihaijun <shihaijun@epipechina.com>
2019-08-15 10:16:51 +0800

    Springboot创建

---
+++ src/main/java/com/cjbs/demo/web/rest/HelloController.java
@@ -0,0 +1,18 @@
+package com.cjbs.demo.web.rest;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/api")
+public class HelloController {
+
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String sayHello(){
+        return "hello";
+    }
+}
